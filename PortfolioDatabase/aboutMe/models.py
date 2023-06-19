3a8fdcbd4ca56860a142eb69412a69a05a537a47
Alex Lustig <exoxander15@gmail.com>
2023-06-19 15:09:16 -0600

    images added and pages styled

--- PortfolioDatabase/aboutMe/models.py
+++ PortfolioDatabase/aboutMe/models.py
@@ -8,6 +8,7 @@ class Hobby(models.Model):
 
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
+    img = models.CharField(max_length=500, default="https://steamuserimages-a.akamaihd.net/ugc/106232967022720363/48DED32DECDB08B6A6377E2AC750277BCF2330A0/")
 
 class Portfolio(models.Model):
 
@@ -16,3 +17,8 @@ class Portfolio(models.Model):
 
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
+    img = models.CharField(max_length=500, default="https://steamuserimages-a.akamaihd.net/ugc/106232967022720363/48DED32DECDB08B6A6377E2AC750277BCF2330A0/")
+
+    #default image
+    #https://steamuserimages-a.akamaihd.net/ugc/204179658730228724/7DA3284CED8D923FECBC5BBB11D021B5842FE4A0/
+    #https://steamuserimages-a.akamaihd.net/ugc/106232967022720363/48DED32DECDB08B6A6377E2AC750277BCF2330A0/
