beb26ed6f77de52af94f8992aef17e3c9fb41ceb
Alex Lustig <exoxander15@gmail.com>
2023-06-19 12:31:14 -0600

    templates done, links done, details pages partially done

--- PortfolioDatabase/aboutMe/urls.py
+++ PortfolioDatabase/aboutMe/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("home", views.home, name="home"),
     path("contact/", views.contact, name="contact"),
     path("hobbies/", views.hobbies, name="hobbies"),
     path("portfolio/", views.portfolio, name="portfolio"),
+    path("hobbies/<int:item_id>", views.hobbyDetails, name="hobbyDetails"),
+    path("portfolio/<int:item_id>", views.portfolioDetails, name="portfolioDetails")
 ]
--- PortfolioDatabase/aboutMe/views.py
+++ PortfolioDatabase/aboutMe/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.template import loader
 
 # Create your views here.
 def home(request):
-    reply = "Welcome to my Portfolio site. I am a CS student at Weber State University in my final semester."
-
-    return HttpResponse(reply)
+    
+    return render(request, "PortfolioDatabase/home.html")
 
 def contact(request):
     reply = "Contact: Alexanderlustig@mail.weber.edu"
@@ -14,11 +14,21 @@ def contact(request):
     return HttpResponse(reply)
 
 def hobbies(request):
-    reply = Hobby.objects.all()
-
-    return HttpResponse(reply)
+    item_list = Hobby.objects.all()
+    context = {"item_list":item_list,}
+    return render(request, "PortfolioDatabase/hobbies.html", context)
 
 def portfolio(request):
-    reply = Portfolio.objects.all()
+    item_list = Portfolio.objects.all()
+    context = {"item_list":item_list,}
+    return render(request, "PortfolioDatabase/portfolio.html",context)
 
-    return HttpResponse(reply)
+def hobbyDetails(request, item_id):
+    item = Hobby.objects.get(pk=item_id)
+    context = {"item":item,}
+    return render(request, "PortfolioDatabase/hobbyDetails.html", context)
+
+def portfolioDetails(request, item_id):
+    item = Portfolio.objects.get(pk=item_id)
+    context = {"item":item,}
+    return render(request, "PortfolioDatabase/portfolioDetails.html", context)
